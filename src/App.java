cd0b7aa955fe49bc570a434c744e11761bc7f5d9
Gustavo-45 <gustavoperesfigueredo2005@gmail.com>
2025-04-28 11:56:57 -0300

    aula1

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+//import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
             System.out.println("\t\tJEstatstics");
             System.out.print("-- Controle de dados empresarial --");
             System.out.print(" 2025");
-
+            //ArrayList lista = new ArrayList();
     }
 }

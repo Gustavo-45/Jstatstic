ee0f087d76384c5d28fbf9b965567eb503983a56
Gustavo-45 <gustavoperesfigueredo2005@gmail.com>
2025-04-28 10:30:22 -0300

    semana1

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+            System.out.println("\t\tJEstatstics");
+            System.out.print("-- Controle de dados empresarial --");
+            System.out.print(" 2025");
+
+    }
+}

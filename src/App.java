b849b51fdcfa5f5052d97d8a78be504008b1ca6f
Gustavo-45 <gustavoperesfigueredo2005@gmail.com>
2025-04-30 09:10:22 -0300

    aula-logica1

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
             int opcao;
             JOptionPane.showMessageDialog(null, "O jogo da estatistica: \nJStatistics", "JStatistics",JOptionPane.DEFAULT_OPTION);
-            opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Vamos começar o jogo? \nEscolha a estrategia: \n1.Populacional 2.Amostral","Fase: 1", JOptionPane.QUESTION_MESSAGE));
+            opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Vamos começar o jogo? \nEscolha a estrategia: \n1.Populacional \n2.Amostral","Fase: 1", JOptionPane.QUESTION_MESSAGE));
             JOptionPane.showMessageDialog(null, "Voce escolheu: "+opcao, "Fase: 1", JOptionPane.INFORMATION_MESSAGE);
 
             String opcaoVariavel = JOptionPane.showInputDialog(null, "Qual o tipo de variavel você deseja cadastrar?: \n1.Quantitativo\n2.Qualitativo","Fase: 2", JOptionPane.QUESTION_MESSAGE);

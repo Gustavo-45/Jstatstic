ef5fc25feb8d6ff030726467735651a9fcadc50d
Gustavo-45 <gustavoperesfigueredo2005@gmail.com>
2025-05-09 09:20:44 -0300

    aula-logica7.ex1

--- src/App.java
+++ src/App.java
@@ -1,20 +1,25 @@
 import javax.swing.JOptionPane;
+import javax.swing.event.DocumentEvent;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-      //Classe é um conjunto de objetos  
-        Mamiferos m1 = new Mamiferos();
-        m1.setNomeCientifico("Bos Tauros");
-        JOptionPane.showMessageDialog(null, "Nome cientifico: "+m1.getNomeCientifico(), "VACA", 1);
-
-        Mamiferos m2 = new Mamiferos();
-        m2.setNomeCientifico("Macrofilos");
-        JOptionPane.showMessageDialog(null, "Nome cientifico: "+m2.getNomeCientifico(), "CANGURU", 1);
-
-        Mamiferos m3 = new Mamiferos();
-        m3.setNomeCientifico("Trichichus");
-        JOptionPane.showMessageDialog(null, "Nome cientifico: "+m3.getNomeCientifico(), "PEIXE BOI", 1);
-
+      JOptionPane.showMessageDialog(null,"\t             Alô, Jardim!","JARDIM",JOptionPane.INFORMATION_MESSAGE);
+      int i = 0;
+      String nome = "";
+      String endereco;
+      while (i == 0){
+      nome = JOptionPane.showInputDialog(null,"Qual seu nome?","JARDIM",JOptionPane.QUESTION_MESSAGE);
+      endereco = JOptionPane.showInputDialog(null,"Qual seu endereço?","JARDIM",JOptionPane.QUESTION_MESSAGE);
+      int escolha = JOptionPane.showConfirmDialog(null, "Seu nome é: "+nome+"\n E seu endereço é o: "+endereco , "JARDIM", JOptionPane.YES_NO_CANCEL_OPTION);
+        if(escolha ==JOptionPane.NO_OPTION){
+          JOptionPane.showMessageDialog(null, "Desculpe pelo engano, me informe novamente", "JARDIM", JOptionPane.WARNING_MESSAGE);
+        }else{
+          i++;
+        }
+      }
+      Double largura = Double.parseDouble(JOptionPane.showInputDialog(null, "Olá "+nome+"\nQual é a largura do seu jardim?", "JARDIM", JOptionPane.QUESTION_MESSAGE));
+      Double comprimento = Double.parseDouble(JOptionPane.showInputDialog(null, "Olá "+nome+"\nQual é o comprimento do seu jardim?", "JARDIM", JOptionPane.QUESTION_MESSAGE));
+      Double area = comprimento * largura;
+      JOptionPane.showMessageDialog(null, "A área do seu jardim é de:"+area+"m2", "JARDIM", JOptionPane.INFORMATION_MESSAGE);
     }
 }

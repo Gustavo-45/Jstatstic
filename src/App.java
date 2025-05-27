5d95ae42db44c4b47bf4028ef8d45c82a4e15576
Gustavo-45 <gustavoperesfigueredo2005@gmail.com>
2025-05-27 10:01:36 -0300

    Agora esta certo

--- src/App.java
+++ src/App.java
@@ -3,5 +3,7 @@ public class App {
         Exercicio ex = new Exercicio();
         ex.nome = "kiko";
         ex.descricao = "Exercício de aquecimento, imitar o kiko oscilando e a lingua vibrando";
+        ex.tempoMin = 3;
+        ex.som ="sons/kiko.mp3";
     }
 }
